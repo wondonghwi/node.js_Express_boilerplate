e7abcae662235031ee6b8e125820f0cfdd6a5d58
원동휘 <donghwi1289@gmail.com>
2021-06-08 00:09:00 +0900

    ADD : (express) -> 회원가입추가

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const port = 5000
+const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./User');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://wondonghwi:qwer1234@cluster0.lvvss.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-    useNewUrlParser : true,
-    useUnifiedTopology : true,
-    useCreateIndex : true,
-    useFindAndModify : false
-}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/',(req , res) => res.send('Hello World'))
+const mongoose = require('mongoose');
+mongoose
+  .connect('mongodb+srv://wondonghwi:qwer1234@cluster0.lvvss.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
-app.listen(port , () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
