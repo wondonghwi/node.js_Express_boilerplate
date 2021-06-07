92ac158ca01b44a6c2a7972ad7913c7c027c32bb
원동휘 <donghwi1289@gmail.com>
2021-06-07 23:09:30 +0900

    ADD : mongoDB 세팅 + 연결

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://wondonghwi:qwer1234@cluster0.lvvss.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useCreateIndex : true,
+    useFindAndModify : false
+}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
+
+app.get('/',(req , res) => res.send('Hello World'))
+
+app.listen(port , () => console.log(`Example app listening on port ${port}!`))
